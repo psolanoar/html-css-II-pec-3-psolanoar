291bf9a0c030fefae350ea56822b056b70e184f3
psolanoar <psolanoar@uoc.edu>
2024-06-02 13:20:10 +0200

    Blog page base transformation style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-brown': '#78350f',
+      },
+      spacing: {
+        '300px': '300px',
+      },
+      padding: {
+        '20': '20%',
+      },
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+      }
+    },
   },
   plugins: [],
 }
